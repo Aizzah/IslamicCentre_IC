e0fa9441179e6a3d5b786f525f3db1d4f820a8ca
Sabilla290 <SABILLA290@GMAIL.COM>
2016-11-20 20:56:55 +0700

    comitte button alquran

--- app/src/main/java/id/sch/smktelkom_mlg/xiirpl505152535/islamiccentre_ic/alquran/alquran.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xiirpl505152535/islamiccentre_ic/alquran/alquran.java
@@ -1,7 +1,9 @@
-package id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic;
+package id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.alquran;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.R;
 
 public class alquran extends AppCompatActivity {
 
