6dd40bfa255e8b1daf1dad90f659520996e07051
Sabilla290 <SABILLA290@GMAIL.COM>
2016-11-21 11:44:26 +0700

    comitte button alquran baru lagi

--- app/src/main/java/id/sch/smktelkom_mlg/xiirpl505152535/islamiccentre_ic/alquran/alquran.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xiirpl505152535/islamiccentre_ic/alquran/alquran.java
@@ -1,8 +1,11 @@
 package id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.alquran;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.MainActivity;
 import id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.R;
 
 public class alquran extends AppCompatActivity {
@@ -11,5 +14,12 @@ public class alquran extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alquran);
+
+        findViewById(R.id.a).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(alquran.this, a_alfatihah.class));
+            }
+        });
     }
 }
