55b48fe144ae3b797c43a87119c1a68eece7cf38
Sabilla290 <SABILLA290@GMAIL.COM>
2016-12-01 09:22:48 +0700

    commite icon dan nama

---
+++ app/src/main/java/id/sch/smktelkom_mlg/xiirpl505152535/islamiccentre_ic/alquran/g_alaraf.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.alquran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.R;
+
+public class g_alaraf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_g_alaraf);
+    }
+}
