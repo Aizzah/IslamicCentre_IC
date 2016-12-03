9b77e3219e8fcd9a5c6f889ba8416b86e3b62993
Muhammad Anwari <arimuhammad1199@gmail.com>
2016-12-04 00:16:27 +0700

    initial commit commit2

---
+++ app/src/main/java/id/sch/smktelkom_mlg/xiirpl505152535/islamiccentre_ic/tajwid/idzharsafawi.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.tajwid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.R;
+
+public class idzharsafawi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idzharsafawi);
+    }
+}
