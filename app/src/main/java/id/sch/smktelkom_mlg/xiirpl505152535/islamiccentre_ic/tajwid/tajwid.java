d4d3125cf5850731f3f5ae53ec9ba80619dcdf43
Muhammad Anwari <arimuhammad1199@gmail.com>
2016-12-04 00:45:33 +0700

    initial commit commit2 commit 3(penyelesaian bagian tajwid)

--- app/src/main/java/id/sch/smktelkom_mlg/xiirpl505152535/islamiccentre_ic/tajwid/tajwid.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xiirpl505152535/islamiccentre_ic/tajwid/tajwid.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.tajwid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import id.sch.smktelkom_mlg.xiirpl505152535.islamiccentre_ic.R;
 
@@ -11,5 +13,83 @@ public class tajwid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tajwid);
+
+        findViewById(R.id.aa).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, idghombighunah.class));
+            }
+        });
+
+        findViewById(R.id.ab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, idghombilaghunah.class));
+            }
+        });
+
+        findViewById(R.id.ac).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, idzhar.class));
+            }
+        });
+
+        findViewById(R.id.ad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, iqlab.class));
+            }
+        });
+
+        findViewById(R.id.ae).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, ikhfa.class));
+            }
+        });
+
+        findViewById(R.id.af).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, idghommimi.class));
+            }
+        });
+
+        findViewById(R.id.ag).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, ikhfasafawi.class));
+            }
+        });
+
+        findViewById(R.id.ah).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, idzharsafawi.class));
+            }
+        });
+
+        findViewById(R.id.ai).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, idghommutamatsilain.class));
+            }
+        });
+
+        findViewById(R.id.aj).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, idghommutajanisain.class));
+            }
+        });
+
+        findViewById(R.id.ak).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tajwid.this, idghommutaqorribain.class));
+            }
+        });
+
     }
 }
